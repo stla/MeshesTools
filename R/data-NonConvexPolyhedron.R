350f728ddaa2c80a77147347f06a93b0e970909c
stla <laurent_step@yahoo.fr>
2022-10-22 15:29:49 +0200

    non-convex polyhedron

---
+++ R/data-NonConvexPolyhedron.R
@@ -0,0 +1,7 @@
+#' @title A mesh of a non-convex polyhedron
+#'
+#' @description A list representing a non-convex polyhedron
+#'   with 14 vertices and 24 triangular faces.
+#'
+#' @format A list with fields \code{vertices} and \code{faces}.
+"NonConvexPolyhedron"
